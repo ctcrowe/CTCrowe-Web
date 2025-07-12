0a049f63ee31885f614f97d3717151bc7975ae2a
Charlie Crowe <charlietcrowe@gmail.com>
2025-07-12 13:26:34 -0700

    Update SingleHeadAttention.js

--- SingleHeadAttention.js
+++ SingleHeadAttention.js
@@ -1,33 +1,33 @@
-import TransformerHelper as helper from "TransformerHelper.js";
+import TransformerHelper as h from "TransformerHelper.js";
 
 class SingleHeadAttention {
   constructor(dModel) {
     this.dModel = dModel;
-    this.Wq = helper.randMatrix(dModel);
-    this.Wk = helper.randMatrix(dModel);
-    this.Wv = helper.randMatrix(dModel);
+    this.Wq = h.randMatrix(dModel);
+    this.Wk = h.randMatrix(dModel);
+    this.Wv = h.randMatrix(dModel);
   }
 
   forward(x) {
     this.x = x;
-    this.Q = helper.matMul(x, this.Wq);
-    this.K = helper.matMul(x, this.Wk);
-    this.V = helper.matMul(x, this.Wv);
+    this.Q = h.matMul(x, this.Wq);
+    this.K = h.matMul(x, this.Wk);
+    this.V = h.matMul(x, this.Wv);
 
-    const K_T = helper.transpose(this.K);
-    this.scores = helper.matMul(this.Q, K_T);
+    const K_T = h.transpose(this.K);
+    this.scores = h.matMul(this.Q, K_T);
     const scale = Math.sqrt(this.dModel);
 
     this.scaledScores = this.scores.map(row => row.map(v => v / scale));
-    this.probs = this.scaledScores.map(row => helper.softmax(row));
-    this.output = helper.matMul(this.probs, this.V);
+    this.probs = this.scaledScores.map(row => h.softmax(row));
+    this.output = h.matMul(this.probs, this.V);
 
     return this.output;
   }
 
 
   backward(dout) {
-    const [dprobs, dV] = helper.matMulBackward(dout, this.probs, this.V);
+    const [dprobs, dV] = h.dMatMul(dout, this.probs, this.V);
     const dScaled = dprobs.map((gradRow, i) =>
       gradRow.map((grad, j) => {
         const p = this.probs[i][j];
@@ -36,12 +36,12 @@ class SingleHeadAttention {
     );
 
     const dscores = dScaled.map(row => row.map(v => v / Math.sqrt(this.dModel)));
-    const [dQ, dKT] = helper.matMulBackward(dscores, this.Q, helper.transpose(this.K));
-    const dK = helper.transpose(dKT);
+    const [dQ, dKT] = h.dMatMul(dscores, this.Q, h.transpose(this.K));
+    const dK = h.transpose(dKT);
 
-    const [dxQ, dWq] = helper.matMulBackward(dQ, this.x, this.Wq);
-    const [dxK, dWk] = helper.matMulBackward(dK, this.x, this.Wk);
-    const [dxV, dWv] = helper.matMulBackward(dV, this.x, this.Wv);
+    const [dxQ, dWq] = h.dMatMul(dQ, this.x, this.Wq);
+    const [dxK, dWk] = h.dMstMul(dK, this.x, this.Wk);
+    const [dxV, dWv] = h.dMatMul(dV, this.x, this.Wv);
 
     this.dWq = dWq;
     this.dWk = dWk;
@@ -56,8 +56,8 @@ class SingleHeadAttention {
 
 
   update(lr) {
-    this.Wq = helper.updateParams(this.Wq, this.dWq, lr);
-    this.Wk = helper.updateParams(this.Wk, this.dWk, lr);
-    this.Wv = helper.updateParams(this.Wv, this.dWv, lr);
+    this.Wq = h.updateParams(this.Wq, this.dWq, lr);
+    this.Wk = h.updateParams(this.Wk, this.dWk, lr);
+    this.Wv = h.updateParams(this.Wv, this.dWv, lr);
   }
 }
